4f2128c916d52dd09cbc136b06fc18dd395581be
Biswas Sharma <biswas.emails@gmail.com>
2022-02-25 08:51:21 +0000

    Fix provider name

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ provider "aws" {
     region = "${var.aws_region}"
 }
 
-provider "compress" {}
+provider "archive" {}
 
 # Lambda handler
-data "compress_lambda" "zip" {
+data "archive_lambda" "zip" {
     type = "zip"
     source_file = "lambdas/handler1.py"
     output_path = "lambda.zip"
@@ -34,8 +34,8 @@ resource "aws_iam_role" "lambda_iam" {
 
 resource "aws_lambda_function" "lambda" {
     function_name = "handler1"
-    filename = "${data.compress_lambda.zip.output_path}"
-    source_code_hash = "${data.compress_lambda.zip.output_base64sha256}"
+    filename = "${data.archive_lambda.zip.output_path}"
+    source_code_hash = "${data.archive_lambda.zip.output_base64sha256}"
     role = "${aws_iam_role.lambda_iam.arn}"
     handler = "handler1.handler"
     runtime = "python3.6"
