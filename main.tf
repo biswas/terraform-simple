8992864ea881cdbc9cc5e920b4ed3af83eacee76
Biswas Sharma <biswas.emails@gmail.com>
2022-03-04 18:21:59 +0000

    Define lambda function name as variable to fix cyclic dependencies between resources

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_iam_role" "lambda_iam3" {
 }
 
 resource "aws_lambda_function" "lambda" {
-  function_name    = "handler1"
+  function_name    = var.lambda_function_name
   filename         = data.archive_file.zip.output_path
   source_code_hash = data.archive_file.zip.output_base64sha256
   role             = aws_iam_role.lambda_iam3.arn
@@ -62,7 +62,7 @@ resource "aws_lambda_function" "lambda" {
 }
 
 resource "aws_cloudwatch_log_group" "lgroup1" {
-  name              = "/aws/lambda/${aws_lambda_function.lambda.function_name}"
+  name              = "/aws/lambda/${var.lambda_function_name}"
   retention_in_days = 2
 }
 
--- variables.tf
+++ variables.tf
@@ -2,3 +2,7 @@ variable "aws_region" {
   description = "Relevant cloud provider region."
   default     = "us-east-1"
 }
+
+variable "lambda_function_name" {
+  default = "handler1"
+}
